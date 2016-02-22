3ac594d18c086a06a5dcbdf9496b29e8d29afd89
townse33 <jtow@live.co.uk>
2016-02-22 17:17:04 +0000

    OBJ importer implemented

--- C++WIP/Project1/Project1/GLTest.cpp
+++ C++WIP/Project1/Project1/GLTest.cpp
@@ -1,56 +1,30 @@
-#include <GL/freeglut.h> //Includes the FreeGLUT library, this contains OpenGL and GLU libraries and provides a higher-level API for OpenGL graphics rendering
+#include <GL/freeglut.h>
 #include<vector>
 #include <string>
 #include <fstream>
-#include <iostream>
 #include <sstream>
-#include <algorithm>
 
 using namespace std;
 
-int init_width = 320, init_height = 320; //Initial window resolution variables
+int init_width = 1200, init_height = 800; //Initial window resolution variables
 int winPosX = 0, winPosY = 0; //Initial window position variables
 char winTitle[] = "Project 2 ALL"; //Window caption
 float angle = 0.0f; //Debug variable for spinning models
 float fov = 45.0f; //Camera's Field Of View in degrees
 
-void renderScene(void) {
-	//Function to render all objects in the current scene
 
-	// Clear Color and Depth Buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+vector<vector<float>> objLoad(string path) { //Our OBJ 3D model loading function 
 
-	// Reset transformations
-	glLoadIdentity();
-	// Set the camera
-	gluLookAt(0.0f, 0.0f, 10.0f,
-		0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f);
 
-	glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-
-	glColor3f(1.0f, 0.0f, 0.0f);
-
-	glutWireSphere(3, 20, 20);
-
-	angle += 0.1f;
-
-	glutSwapBuffers();
-}
-
-void objLoad(string path) { //Our OBJ 3D model loading function 
-
-	
 	vector<vector<float>> vertices, UV, normals; //Store XYZ vertex co-ordinates using a vector in a vector
-	int i=1;
-
+	unsigned i = 0;
 	vector<vector<int>> vertexFace;
+
+	vertices.push_back(vector<float>());
 
 	string line;
 
-	ifstream objFile (path); //Open the file path given to the function
+	ifstream objFile(path); //Open the file path given to the function
 
 
 	while (getline(objFile, line)) {
@@ -58,7 +32,6 @@ void objLoad(string path) { //Our OBJ 3D model loading function
 		string h = line.substr(0, 2); //Line header, tells us what the line is defining
 
 		if (h == "v ") { //If line contains vertex definition
-			cout << "vertex" << endl;
 
 			vector<string> temp_vector; //Vector to hold strings for this method
 			vector<float> temp_vector2; //Vector to hold floats for this method
@@ -77,18 +50,17 @@ void objLoad(string path) { //Our OBJ 3D model loading function
 
 		}
 		else if (h == "vt") { //If line contains UV definition
-			cout << "UV" << endl;
 
-		
+
 		}
 		else if (h == "vn") { //If line contains vertex normal definition
-			
+
 		}
 		else if (h == "f ") { //If line contains face definition
 
-			
+
 			vector<string> temp_vector; //Vector to hold strings for this method
-			vector<int> temp_vector2; //Vector to hold ints for this method
+			vector<int> temp_vector2, temp_vector3; //Vector to hold ints for this method
 			string temp_line = line.substr(2), temp_str; //Remove line header
 
 			stringstream ss(temp_line); //Make the current line act as an input stream
@@ -114,24 +86,32 @@ void objLoad(string path) { //Our OBJ 3D model loading function
 				}
 
 			}
-			vertexFace[i].push_back(temp_vector2[0,2,4]); //Store the 3 vertex IDs in a vector
 
-			for each(int k in temp_vector2) {
-				cout << k << ",";
+			if (temp_vector2.size() != 0) {
+
+				temp_vector3.push_back(temp_vector2[0]);
+				temp_vector3.push_back(temp_vector2[2]);
+				temp_vector3.push_back(temp_vector2[4]);
+				vertexFace.push_back(temp_vector3);
+
 			}
 
-			cout << endl;
-			
 		}
-		else if (h == "# ") {
-			cout << "comment" << endl;
-		}
-
-		cout << line << endl;
 		i++;
 	}
 
+	vector<vector<float>> finalVec;
+	for each(vector<int> vecI in vertexFace) {
+		for each(int id in vecI) {
+			finalVec.push_back(vertices[id]);
+		}
+	}
+
+	return finalVec;
+
 }
+
+
 
 
 
@@ -156,20 +136,55 @@ void changeSize(int w, int h) {
 
 }
 
+void renderScene(void) {
+	//Function to render all objects in the current scene
+
+	// Clear Color and Depth Buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Reset transformations
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(0.0f, 0.0f, 10.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
+	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+
+	//glTranslatef(0, 0, 0);
+
+	//glColor3f(1.0f, 0.0f, 0.0f);
+
+	vector<vector<float>> arrayBuffer = objLoad("test.obj");
+
+
+	glBegin(GL_TRIANGLES);
+	for each(vector<float> vertex in arrayBuffer) {
+	glVertex3f(vertex[0], vertex[1], vertex[2]);
+	}
+	glEnd();
+
+	angle += 0.1f;
+
+	glutSwapBuffers();
+
+}
 
 int main(int argc, char** argv) //Main function, parameters are used for initialising GLUT
 {
 	glutInit(&argc, argv); //Initialises GLUT, parameters necessary for initiliasation
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);  //DEPTH: Enables 3D, DOUBLE: Enables double buffering, RGBA: Colour with alpha
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE| GLUT_RGBA);  //DEPTH: Enables 3D, DOUBLE: Enables double buffering, RGBA: Colour with alpha
 	glutInitWindowSize(init_width, init_height);
 	glutInitWindowPosition(winPosX, winPosY);
 	glutCreateWindow(winTitle);
 
+	//glEnable(GL_DEPTH_TEST);
+
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
 	glutIdleFunc(renderScene);
-
-	objLoad("test.obj");
 
 	glutMainLoop();
 	return 1;
